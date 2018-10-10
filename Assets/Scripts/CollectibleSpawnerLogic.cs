cf0812a5fd7b29fe566958a80af0132bf75e06ad
vrnico <nico.daunt@gmail.com>
2018-10-10 11:25:56 -0700

    Add Scripts

---
+++ Assets/Scripts/CollectibleSpawnerLogic.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleSpawnerLogic : MonoBehaviour {
+
+	public GameObject collectible;
+	public GameObject badCollectible;
+
+	public float collectibleSpawnTime = 1.0f;
+	public float badCollectibleSpawnTime = 2.0f;
+
+	public bool canSpawnCollectible = true;
+	public bool canSpawnBadCollectible = true;
+
+	float randomXCollectible = 0.0f;
+	float randomYCollectible = 0.0f;
+
+	float randomXBadCollectible = 0.0f;
+	float randomYBadCollectible = 0.0f;
+
+	float maxX = 12.5f;
+	float maxY = 9.0f;
+
+	void Start () {
+		StartCoroutine (spawnCollectibles ());
+		StartCoroutine (spawnBadCollectibles ());
+	}
+	
+
+	void Update () {
+		
+	}
+
+	IEnumerator spawnCollectibles() {
+		while (canSpawnCollectible == true){
+			spawnCollectible ();
+			yield return new WaitForSeconds (collectibleSpawnTime);
+		}
+	}
+
+	IEnumerator spawnBadCollectibles (){
+		while ( canSpawnBadCollectible == true){
+			spawnBadCollectible ();
+			yield return new WaitForSeconds (badCollectibleSpawnTime);
+
+		}
+	}
+
+
+	void spawnCollectible () {
+		randomXCollectible = Random.Range (-maxX, maxX);
+		randomYCollectible = Random.Range (-maxY, maxY);
+
+		Instantiate (collectible, new Vector3 (randomXCollectible, randomYCollectible, 0), Quaternion.identity);
+	}
+
+	void spawnBadCollectible () {
+		randomXBadCollectible = Random.Range (-maxX, maxX);
+		randomYBadCollectible = Random.Range (-maxY, maxY);
+
+		Instantiate (badCollectible, new Vector3 (randomXBadCollectible, randomYBadCollectible, 0), Quaternion.identity);
+	}
+}
---
+++ Assets/Scripts/PlayerLogic.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLogic : MonoBehaviour {
+
+	public float playerSpeed = 2000.0f;
+
+	ScoreLogic addToScore;
+
+	void Start () {
+		hideMouseCursor ();
+	}
+	
+
+	void Update () {
+		movePlayerToMousePosition ();
+	}
+
+	void movePlayerToMousePosition(){
+		var tempMousePosition = Input.mousePosition;
+		tempMousePosition.z = transform.position.z - Camera.main.transform.position.z;
+		tempMousePosition = Camera.main.ScreenToWorldPoint (tempMousePosition);
+		transform.position = Vector3.MoveTowards (transform.position, tempMousePosition, playerSpeed * Time.deltaTime);
+	}
+
+	void hideMouseCursor (){
+		Cursor.visible = false;
+	}
+
+	void OnCollisionEnter2D (Collision2D tempCollision) {
+		if (tempCollision.gameObject.tag == "Collectible") {
+			callAddToScoreScript ();
+
+			Destroy (tempCollision.gameObject);
+			
+		}
+
+		if (tempCollision.gameObject.tag == "BadCollectible"){
+			resetGame ();
+		}
+	}
+
+
+	void callAddToScoreScript () {
+		addToScore = GameObject.FindGameObjectWithTag ("GUI").GetComponent<ScoreLogic> ();
+		addToScore.addToScoreVOID ();
+	}
+
+	void resetGame() {
+		SceneManager.LoadScene ("GameScene");
+	}
+}
